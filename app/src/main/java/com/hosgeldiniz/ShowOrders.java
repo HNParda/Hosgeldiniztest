ff9aa36a166cff2391454210a184e535a53ecb9d
HNParda <memiliarda5@gmail.com>
2022-05-31 08:39:12 +0300

    .

--- app/src/main/java/com/hosgeldiniz/ShowOrders.java
+++ app/src/main/java/com/hosgeldiniz/ShowOrders.java
@@ -2,16 +2,27 @@ package com.hosgeldiniz;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ListView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.hosgeldiniz.utils.Adapter.OrderMenuAdapter;
+import com.hosgeldiniz.utils.Adapter.OrderMenuAdapterR;
 import com.hosgeldiniz.utils.DefActivity;
 @SuppressLint("SetTextI18n")
 public class ShowOrders extends DefActivity {
+
+    RecyclerView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_orders);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        list = (RecyclerView) findViewById(R.id.list);
+        list.setLayoutManager(layoutManager);
+        list.setAdapter(new OrderMenuAdapterR(this));
     }
-// https://stackoverflow.com/questions/3240331/horizontal-listview-in-android
+
 }
